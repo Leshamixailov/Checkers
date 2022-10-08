c52992cc04fb0080609f60186f033d1216183e48
Персик Сочный <rweerew@inbox.ru>
2022-10-08 22:26:51 +0300

    Отображение шахматной доски

--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,41 @@ namespace Checkers
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Pen Black = new Pen(Color.Green, 1);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            int countHorizontal = pictureBox1.Width / 8;
+            int countVertical = pictureBox1.Height / 8;
+           
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                for (int i = 0; i < countHorizontal; i++)
+                {
+                    for (int j = 0; j < countVertical; j++)
+                    {
+                        if (i%2==1 )
+                        {   if (j % 2 == 1)
+                            {
+                                g.FillRectangle(Brushes.Black, i * 100, j * 100, 100, 100);
+                                g.DrawRectangle(Black, i * 100, j * 100, 100, 100);
+                            }
+                         
+                        }
+                        if (i % 2 == 0)
+                        {
+                            if (j % 2 == 0)
+                            {
+                                g.FillRectangle(Brushes.Black, i * 100, j * 100, 100, 100);
+                                g.DrawRectangle(Black, i * 100, j * 100, 100, 100);
+                            }
+                        }
+
+                    }
+                }
+            }
+            
+        }
     }
 }
